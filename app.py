3c58d1602d929d00076732ea1054dcd1e9e3e63e
CalvoM <calvomaks@gmail.com>
2019-09-17 14:11:45 +0300

    Check the entry text

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask,render_template,request
 
 site     = Flask(__name__)
 response = "" 
+userEmail = ""
+userName  = ""
+isEmail    = False #checking if email was provided
+isUserName = False #checking if username was provided
 
 # Landing page for the site
 @site.route('/')
@@ -10,8 +14,12 @@ def home():
 
 # Implementing the ussd functionality
 @site.route('/app/ussd',methods=['POST','GET'])
-def ussd_callback():
+def ussdCallback():
     global response
+    global userEmail
+    global userName
+    global isEmail
+    global isUserName
     # getting operation data for the app
     sessionId = request.values.get("sessionId", None)
     serviceCode = request.values.get("serviceCode", None)
@@ -19,7 +27,7 @@ def ussd_callback():
     userResp = request.values.get("text", "default")
 
     if userResp == "":
-        response = "CON Enter your email:\n"
+        response = "CON What would you like us to call you:\n"
     return response
 
 if __name__ == "__main__":
