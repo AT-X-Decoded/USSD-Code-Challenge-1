742dbbefc6958e34623e7a0999f18acd3bb42525
CalvoM <calvomaks@gmail.com>
2019-09-17 11:41:58 +0300

    Create basic connection

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
-from flask import Flask
+from flask import Flask,render_template,request
 
-site = Flask(__name__)
+site     = Flask(__name__)
+response = "" 
 
+# Landing page for the site
 @site.route('/')
 def home():
-    return "Hello There!"
+    return render_template('index.html')  
 
+# Implementing the ussd functionality
+@site.route('/app/ussd',methods=['POST','GET'])
+def ussd_callback():
+    global response
+    # getting operation data for the app
+    sessionId = request.values.get("sessionId", None)
+    serviceCode = request.values.get("serviceCode", None)
+    phoneNumber = request.values.get("phoneNumber", None)
+    userResp = request.values.get("text", "default")
+
+    if userResp == "":
+        response = "CON Enter your email:\n"
+    return response
 
 if __name__ == "__main__":
     site.run(host='0.0.0.0')
